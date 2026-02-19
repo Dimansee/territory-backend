a7d4944d033afb13d5baff002e12d090a3b7632f
Dimansee <mannchoudhary04@gmail.com>
2026-02-19 22:02:18 +0530

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import sqlite3
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -96,4 +97,5 @@ def territories():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
